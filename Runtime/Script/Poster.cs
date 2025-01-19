9989f277fc858bb2212cf2e531a1cf185219e00b
Nomlas <161704576+nomlasvrc@users.noreply.github.com>
2025-01-19 18:14:42 +0900

    v1.2.3

--- Runtime/Script/Poster.cs
+++ Runtime/Script/Poster.cs
@@ -23,7 +23,7 @@ namespace Nomlas.Poster
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class Poster : UdonSharpBehaviour
     {
-        [NonSerialized] internal readonly string version = "v1.2.2";
+        [NonSerialized] internal readonly string version = "v1.2.3";
         [SerializeField] internal Language language;
         [SerializeField] private VRCUrl[] picUrls;
         [SerializeField] private VRCUrl lengthURL;
